9e7cff593c26f467f0def670d8ae00a6615ba5ce
Walik23 <122877885+Walik23@users.noreply.github.com>
2023-06-06 21:38:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,19 @@
  
   'use strict';
   
-  //adding event listener to button
-  button.addEventListener('click',function(){})
+  //function which defines a part of speech
+  button.addEventListener('click',function(){
+   
+    const text = inputField.value;//capturing text inside input field
+
+    //clearing output field from previous output
+    let outputField = document.querySelector('#output');
+    outputField.innerHTML = '';
+
+    const sentences = text.split(separators).filter(Boolean);//spliting text by regular regex and deleting the empty elements
+    const wordArrays = sentences.map(sentence => sentence.split(' '));//spliting sentences into words
+  
+  
+  })
 
 })()
